fba6559f4c61e5b810a3127843ded68d92d6ac15
Raffi Dary <rraffidh@gmail.com>
2024-09-13 14:14:55 +0700

    Changed context based on name class and npm (Forgot to change context T-T)

--- main/views.py
+++ main/views.py
@@ -24,9 +24,9 @@ def show_main(request):
     mood_entries = MoodEntry.objects.all()
 
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP D',
-        'npm': '2306123456',
+        'name': 'Raffi Dary Hibban',
+        'class': 'PBP E',
+        'npm': '2306203690',
         'mood_entries': mood_entries
     }
 
